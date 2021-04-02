e53e9210e7f75ceeae1602cfe36bb40343df4fcb
oamontoya <79286232+oamontoya@users.noreply.github.com>
2021-04-01 21:05:13 -1000

    Update GPS_SD_Card_Force_Water_IMU.ino
    Encapsulated functions as much as possible. changed formatting and added comments

--- Mega/GPS_SD_Card_Force_Water_IMU/GPS_SD_Card_Force_Water_IMU.ino
+++ Mega/GPS_SD_Card_Force_Water_IMU/GPS_SD_Card_Force_Water_IMU.ino
@@ -28,12 +28,13 @@
 #define BAUD_RATE 115200
 #define BNO055_SAMPLERATE_DELAY_MS 100
 
+//define pin assignments
 #define GPSSerial Serial3
 #define PRESSUREPIN A0
 #define WATERPIN A1
+#define CHIPSELECT 10
 
 //define global variables
-const int chipSelect = 10;
 uint32_t timer = millis();
 String outputString = "";
 String filename = "";
@@ -51,53 +52,127 @@ void setup() {
   // also spit it out
   Serial.begin(BAUD_RATE);
 
+  //initialize orientation sensor
   bno.begin();
-  bmp.begin();
   delay(1000);
   bno.setExtCrystalUse(true);
-  
+
+  //initialize barometric pressure sensor
+  bmp.begin();
+
   initializeGPS();
+
   initializeSD();
 }
 
 void loop() {
-  
+
   readAndParseGPS();
-  
-  // approximately every 2 seconds or so, print out the current stats
+
+  // approximately every second or so, print out the current stats
   if (millis() - timer > 1000) {
     timer = millis(); // reset the timer
-    gpsString();
-    outputString.concat(", ");
-    outputString.concat(analogRead(PRESSUREPIN));
-    outputString.concat(", ");
-    outputString.concat(analogRead(WATERPIN));
-    outputString.concat(", ");
-    outputString.concat(GPS.fix);
-    printCalibrationVal();
-    printQuaternion();
-    printEulerAngle();
-    // open the file. note that only one file can be open at a time,
-    // so you have to close this one before opening another.
-    File dataFile = SD.open(filename, FILE_WRITE);
 
-    // if the file is available, write to it:
-    if (dataFile) {
-      dataFile.println(outputString);
-      Serial.print("Printed to ");
-      Serial.print(filename);
-      Serial.print(" : ");
-      Serial.println(outputString);
-    }
-    // if the file isn't open, pop up an error:
-    else {
-      Serial.print("error opening ");
-      Serial.println(filename);
-    }
-    dataFile.close();
+    //add sensor data to a single output string
+    gpsString();
+    pressureString();
+    waterIntrusionString();
+    calibrationString();
+    quatString();
+    eulerString();
+
+    writeToSD();
     outputString = "";
   }
-    
+}
+
+void initializeGPS() {
+  //initialize GPS
+  Serial.println("Initializing GPS...");
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+
+  //wait until first fix to continue initialization
+  Serial.print("Awaiting GPS fix... ");
+  while (GPS.fix == 0) {
+    readAndParseGPS();
+  }
+  Serial.println("GPS fix found.");
+}
+
+void initializeSD() {
+  //initialize SD
+  Serial.println("Initializing SD...");
+  SD.begin(CHIPSELECT);
+  Serial.println("SD Initialized");
+
+  //generate a filename
+  Serial.println("Generating filename from GPS date/time data...");
+  while (filename == "00000000" || filename == "") {
+    char c = GPS.read();
+    GPS.parse(GPS.lastNMEA());
+
+    filename = "";
+    if (GPS.month < 10) { filename.concat("0"); }
+    filename.concat(GPS.month);
+    if (GPS.day < 10) { filename.concat("0"); }
+    filename.concat(GPS.day);
+    if (GPS.hour < 10) { filename.concat("0"); }
+    filename.concat(GPS.hour);
+    if (GPS.minute < 10) { filename.concat("0"); }
+    filename.concat(GPS.minute);
+  }
+  filename.concat(".csv");
+  Serial.print("Filename ");
+  Serial.print(filename);
+  Serial.println(" created");
+
+  //write header data to SD
+  File dataFile = SD.open(filename, FILE_WRITE);
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println("Date, Time, Latitude, Longitude, GPS Fix, Force, Water Level, Accel, Gyro, Magne, Sys, Quat W, Quat X, Quat Y, Quat Z, Euler X, Euler Y, Euler Z");
+    Serial.print("Header data written to ");
+    Serial.println(filename);
+  }
+  else {
+    Serial.print("Unable to open ");
+    Serial.println(filename);
+  }
+  dataFile.close();
+}
+
+void readAndParseGPS() {
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    //Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+}
+
+void writeToSD() {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open(filename, FILE_WRITE);
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(outputString);
+    Serial.print("Printed to ");
+    Serial.print(filename);
+    Serial.print(" : ");
+    Serial.println(outputString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.print("error opening ");
+    Serial.println(filename);
+  }
+  dataFile.close();
 }
 
 void gpsString() {
@@ -127,72 +202,22 @@ void gpsString() {
     outputString.concat(", ");
     outputString.concat("0");
   }
+
+  outputString.concat(", ");
+  outputString.concat(GPS.fix);
 }
 
-void readAndParseGPS() {
-  // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    //Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
-  }
+void pressureString() {
+  outputString.concat(", ");
+  outputString.concat(analogRead(PRESSUREPIN));
 }
 
-void initializeGPS(){
-  Serial.println("Initializing GPS...");
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
-
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-
-  Serial.print("Awaiting GPS fix... ");
-  while(GPS.fix == 0) { readAndParseGPS(); }
-  Serial.println("GPS fix found.");
+void waterIntrusionString() {
+  outputString.concat(", ");
+  outputString.concat(analogRead(WATERPIN));
 }
 
-void initializeSD(){
-    Serial.println("Initializing SD...");
-    SD.begin(chipSelect);
-    Serial.println("SD Initialized");
-    Serial.println("Generating filename from GPS date/time data...");
-    while(filename == "00000000" || filename == ""){
-      char c = GPS.read();
-      GPS.parse(GPS.lastNMEA());
-      
-      filename = "";
-      if(GPS.month < 10) { filename.concat("0"); }
-      filename.concat(GPS.month);
-      if(GPS.day < 10) { filename.concat("0"); }
-      filename.concat(GPS.day);
-      if(GPS.hour < 10) { filename.concat("0"); }
-      filename.concat(GPS.hour);
-      if(GPS.minute < 10) { filename.concat("0"); }
-      filename.concat(GPS.minute);
-    }
-    filename.concat(".csv");
-    Serial.print("Filename ");
-    Serial.print(filename);
-    Serial.println(" created");
-
-    File dataFile = SD.open(filename, FILE_WRITE);
-
-    // if the file is available, write to it:
-    if (dataFile) {
-      dataFile.println("Date, Time, Latitude, Longitude, Force, Water Level, GPS Fix, Accel, Gyro, Magne, Sys, Quat W, Quat X, Quat Y, Quat Z, Euler X, Euler Y, Euler Z");
-      Serial.print("Header data written to ");
-      Serial.println(filename);
-    }
-    else {
-      Serial.print("Unable to open ");
-      Serial.println(filename);
-    }
-    dataFile.close();   
-}
-
-void printCalibrationVal() {
+void calibrationString() {
   uint8_t accel, gyro, magne, sys = 0;
   bno.getCalibration(&sys, &gyro, &accel, &magne);
   outputString.concat(", ");
@@ -206,7 +231,7 @@ void printCalibrationVal() {
   outputString.concat(", ");
 }
 
-void printQuaternion() {
+void quatString() {
   imu::Quaternion quat = bno.getQuat();
   outputString.concat(quat.w());
   outputString.concat(", ");
@@ -218,7 +243,7 @@ void printQuaternion() {
   outputString.concat(", ");
 }
 
-void printEulerAngle() {
+void eulerString() {
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   outputString.concat(euler.x());
   outputString.concat(", ");
